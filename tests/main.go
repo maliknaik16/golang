4908e783cedbe615095f461951799bbc125e6111
Malik Naik <maliknaik16@gmail.com>
2020-01-20 01:26:14 +0530

    Add main.go

---
+++ tests/main.go
@@ -0,0 +1,14 @@
+
+package main
+
+import "fmt"
+
+// Source https://tutorialedge.net/golang/intro-testing-in-go/
+// Calculate returns x + 2
+func Calculate(x int) int {
+	return x + 2 + 1
+}
+
+func main() {
+	fmt.Println("Hello, World")
+}
